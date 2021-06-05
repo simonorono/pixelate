e8a4c521a7333b9c498916327133df9bdd6840b1
Simón Oroño <simonorono@protonmail.com>
2021-06-05 16:16:24 -0400

    Start a Vite project
    Using editorconfig, Vite, React, PostCSS and TailwindCSS.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./index.html', './src/**/*.{ts,tsx}'],
+  mode: 'jit',
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors,
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
