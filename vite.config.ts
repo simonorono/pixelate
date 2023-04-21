0834b629b10e78b28991be00e1828487b999444e
Simón Oroño <simonorono@protonmail.com>
2023-04-21 07:30:57 -0400

    Replace @vitejs/plugin-react with @vitejs/plugin-react-swc

--- vite.config.ts
+++ vite.config.ts
@@ -15,9 +15,9 @@
  */
 
 import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteReact()]
+  plugins: [react()]
 })
